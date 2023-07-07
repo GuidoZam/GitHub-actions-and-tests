592ff6efd563d5c64e61573c0eb04d8eee632634
guido-zambarda <guido@piasys.it>
2023-07-07 17:15:04 +0200

    Added test methods

---
+++ ActionsAndTests/TestMe.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace ActionsAndTests;
+
+public class TestMe
+{
+    [SetUp]
+    public async Task Setup()
+    {
+    }
+
+    [TearDown]
+    public async Task Teardown()
+    {
+    }
+
+    [Test]
+    public async Task SimpleTestMethod()
+    {
+        Assert.IsTrue(true);
+    }
+
+    [Test]
+    public async Task SimpleTestMethod2()
+    {
+        Assert.IsTrue(true);
+    }
+}
